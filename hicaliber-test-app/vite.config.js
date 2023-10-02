693f8de83113e26bd67303d1f31eb9b3602cab54
ivan.semenov.mob <ivan.semenov.mob@gmail.com>
2023-10-02 10:16:46 +0200

    element plus installation

--- hicaliber-test-app/vite.config.js
+++ hicaliber-test-app/vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
     plugins: [
@@ -8,6 +11,12 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+        }),
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],
         }),
     ],
 });
